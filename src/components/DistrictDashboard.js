9e4e3c4620989c8f064dfdd0666082bd1fa0896b
Bodhish Thomas <bodhish@gmail.com>
2020-06-22 02:04:37 +0530

    Update design for district dashboard

--- src/components/DistrictDashboard.js
+++ src/components/DistrictDashboard.js
@@ -71,11 +71,37 @@ export default function DistrictDashboard() {
     (f) => f.districtId === filterDistrict.id
   );
 
+  let totalVentilator = filteredFacilities
+    .map((f) => f.capacity[20]?.total_capacity || 0)
+    .reduce((a, b) => a + b, 0);
+  let totalIcu = filteredFacilities
+    .map((f) => f.capacity[10]?.total_capacity || 0)
+    .reduce((a, b) => a + b, 0);
+  let totalBed = filteredFacilities
+    .map((f) => f.capacity[1]?.total_capacity || 0)
+    .reduce((a, b) => a + b, 0);
+  let totalRoom = filteredFacilities
+    .map((f) => f.capacity[3]?.total_capacity || 0)
+    .reduce((a, b) => a + b, 0);
+
+  let ventilatorUsed = filteredFacilities
+    .map((f) => f.capacity[20]?.current_capacity || 0)
+    .reduce((a, b) => a + b, 0);
+  let icuUsed = filteredFacilities
+    .map((f) => f.capacity[10]?.current_capacity || 0)
+    .reduce((a, b) => a + b, 0);
+  let bedUsed = filteredFacilities
+    .map((f) => f.capacity[1]?.current_capacity || 0)
+    .reduce((a, b) => a + b, 0);
+  let roomUsed = filteredFacilities
+    .map((f) => f.capacity[3]?.current_capacity || 0)
+    .reduce((a, b) => a + b, 0);
+
   return (
     <div id="mapid" className="max-w-7xl mx-auto">
       <div>
         <div className="relative inline-block text-left">
-          <div>
+          <div className="m-4">
             <span className="rounded-md shadow-sm">
               <button
                 type="button"
@@ -125,90 +151,180 @@ export default function DistrictDashboard() {
             </div>
           )}
         </div>
+        <div className="bg-gray-50 pt-12 sm:pt-16">
+          <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="max-w-4xl mx-auto text-center">
+              <h2 className="text-3xl leading-9 font-extrabold text-gray-900 sm:text-4xl sm:leading-10">
+                {filterDistrict.name} District Dashboard
+              </h2>
+              <p className="mt-3 text-xl leading-7 text-gray-500 sm:mt-4">
+                Capacity Management Dashboard
+              </p>
+            </div>
+          </div>
+          <div className="mt-10 pb-12 bg-white sm:pb-16">
+            <div>
+              <div className=" max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="max-w-4xl mx-auto pt-4">
+                  <dl className="rounded-lg bg-white shadow-lg mt-6">
+                    <div className="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r sm:border-t">
+                      <dt
+                        className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500"
+                        id="item-1"
+                      >
+                        Hospitals
+                      </dt>
+                      <dd
+                        className="order-1 text-5xl leading-none font-extrabold text-indigo-600"
+                        aria-describedby="item-1"
+                      >
+                        {filteredFacilities.length}
+                      </dd>
+                    </div>
 
-        <div className="mt-4 font-bold text-3xl mx-2">Capacity</div>
-        <div className="flex justify-between max-w-5xl mt-4">
-          <div class="bg-white overflow-hidden shadow rounded-lg w-1/4 mx-2">
-            <div class="px-4 py-5 sm:p-6   text-4xl font-semibold text-center">
-              {filteredFacilities
-                .map((f) => f.capacity[20]?.total_capacity || 0)
-                .reduce((a, b) => a + b, 0)}
-            </div>
-            <div class="bg-gray-50 px-4 py-4 sm:px-6   text-center">
-              Number of Ventilators
-            </div>
-          </div>
-          <div class="bg-white overflow-hidden shadow rounded-lg w-1/4 mx-2">
-            <div class="px-4 py-5 sm:p-6   text-4xl font-semibold text-center">
-              {filteredFacilities
-                .map((f) => f.capacity[10]?.total_capacity || 0)
-                .reduce((a, b) => a + b, 0)}
-            </div>
-            <div class="bg-gray-50 px-4 py-4 sm:px-6   text-center">
-              Number of ICU's
-            </div>
-          </div>
-          <div class="bg-white overflow-hidden shadow rounded-lg w-1/4 mx-2">
-            <div class="px-4 py-5 sm:p-6   text-4xl font-semibold text-center">
-              {filteredFacilities
-                .map((f) => f.capacity[1]?.total_capacity || 0)
-                .reduce((a, b) => a + b, 0)}
-            </div>
-            <div class="bg-gray-50 px-4 py-4 sm:px-6   text-center">
-              Number of Beds
-            </div>
-          </div>
-          <div class="bg-white overflow-hidden shadow rounded-lg w-1/4 mx-2">
-            <div class="px-4 py-5 sm:p-6   text-4xl font-semibold text-center">
-              {filteredFacilities
-                .map((f) => f.capacity[3]?.total_capacity || 0)
-                .reduce((a, b) => a + b, 0)}
-            </div>
-            <div class="bg-gray-50 px-4 py-4 sm:px-6   text-center">
-              Number of Rooms
+                    <div className="sm:grid sm:grid-cols-2 ">
+                      <div className="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r sm:border-t">
+                        <dt
+                          className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500"
+                          id="item-1"
+                        >
+                          Ventilators
+                        </dt>
+                        <dd
+                          className="order-1 text-5xl leading-none font-extrabold text-indigo-600"
+                          aria-describedby="item-1"
+                        >
+                          {Math.round((ventilatorUsed / totalVentilator) * 100)}
+                          %
+                        </dd>
+                      </div>
+                      <div className="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r sm:border-t">
+                        <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
+                          ICU
+                        </dt>
+                        <dd className="order-1 text-5xl leading-none font-extrabold text-indigo-600">
+                          {Math.round((icuUsed / totalIcu) * 100)}%
+                        </dd>
+                      </div>
+                      <div className="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r sm:border-t">
+                        <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
+                          Beds
+                        </dt>
+                        <dd className="order-1 text-5xl leading-none font-extrabold text-indigo-600">
+                          {Math.round((bedUsed / totalBed) * 100)}%
+                        </dd>
+                      </div>
+                      <div className="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-t">
+                        <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
+                          Room
+                        </dt>
+                        <dd className="order-1 text-5xl leading-none font-extrabold text-indigo-600">
+                          {Math.round((roomUsed / totalRoom) * 100)}%
+                        </dd>
+                      </div>
+                    </div>
+                  </dl>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="mt-4 font-bold text-3xl mx-2">Occupied</div>
-        <div className="flex justify-between max-w-5xl mt-2">
-          <div class="bg-white overflow-hidden shadow rounded-lg w-1/4 mx-2">
-            <div class="px-4 py-5 sm:p-6   text-4xl font-semibold text-center">
-              {filteredFacilities
-                .map((f) => f.capacity[20]?.current_capacity || 0)
-                .reduce((a, b) => a + b, 0)}
+        <div className="mt-4 font-bold text-3xl mx-2">Capacity</div>
+        <div className="flex flex-col md:flex-row justify-between mt-4">
+          <div className="w-full md:w-1/4 mt-4">
+            <div className="bg-white overflow-hidden shadow rounded-lg mx-2">
+              <div className="px-4 py-5 sm:p-6 text-4xl font-semibold text-center">
+                {totalVentilator}
+              </div>
+              <span className="px-4 py-1 bg-green-400 rounded-r text-white shadow">
+                Current
+              </span>
+              <div className="bg-gray-50 px-4 py-4 sm:px-6 text-center -mt-3">
+                Number of Ventilators
+              </div>
             </div>
-            <div class="bg-gray-50 px-4 py-4 sm:px-6   text-center">
-              Number of Ventilators
-            </div>
-          </div>
-          <div class="bg-white overflow-hidden shadow rounded-lg w-1/4 mx-2">
-            <div class="px-4 py-5 sm:p-6   text-4xl font-semibold text-center">
-              {filteredFacilities
-                .map((f) => f.capacity[10]?.current_capacity || 0)
-                .reduce((a, b) => a + b, 0)}
-            </div>
-            <div class="bg-gray-50 px-4 py-4 sm:px-6   text-center">
-              Number of ICU's
-            </div>
-          </div>
-          <div class="bg-white overflow-hidden shadow rounded-lg w-1/4 mx-2">
-            <div class="px-4 py-5 sm:p-6   text-4xl font-semibold text-center">
-              {filteredFacilities
-                .map((f) => f.capacity[1]?.current_capacity || 0)
-                .reduce((a, b) => a + b, 0)}
-            </div>
-            <div class="bg-gray-50 px-4 py-4 sm:px-6   text-center">
-              Number of Beds
+            <div className="bg-white overflow-hidden shadow rounded-lg mx-2 mt-4">
+              <div className="px-4 py-5 sm:p-6   text-4xl font-semibold text-center">
+                {roomUsed}
+              </div>
+              <span className="px-4 py-1 bg-blue-400 rounded-r text-white shadow">
+                Used
+              </span>
+              <div className="bg-gray-50 px-4 py-4 sm:px-6 text-center -mt-3">
+                Number of Ventilators
+              </div>
             </div>
           </div>
-          <div class="bg-white overflow-hidden shadow rounded-lg w-1/4 mx-2">
-            <div class="px-4 py-5 sm:p-6   text-4xl font-semibold text-center">
-              {filteredFacilities
-                .map((f) => f.capacity[3]?.current_capacity || 0)
-                .reduce((a, b) => a + b, 0)}
+          <div className="w-full md:w-1/4 mt-4">
+            <div className="bg-white overflow-hidden shadow rounded-lg mx-2">
+              <div className="px-4 py-5 sm:p-6   text-4xl font-semibold text-center">
+                {totalIcu}
+              </div>
+              <span className="px-4 py-1 bg-green-400 rounded-r text-white shadow">
+                Current
+              </span>
+              <div className="bg-gray-50 px-4 py-4 sm:px-6 text-center -mt-3">
+                Number of ICU's
+              </div>
             </div>
-            <div class="bg-gray-50 px-4 py-4 sm:px-6   text-center">
-              Number of Rooms
+            <div className="bg-white overflow-hidden shadow rounded-lg mx-2 mt-4">
+              <div className="px-4 py-5 sm:p-6   text-4xl font-semibold text-center">
+                {icuUsed}
+              </div>
+              <span className="px-4 py-1 bg-blue-400 rounded-r text-white shadow">
+                Used
+              </span>
+              <div className="bg-gray-50 px-4 py-4 sm:px-6 text-center -mt-3">
+                Number of Ventilators
+              </div>
+            </div>
+          </div>
+          <div className="w-full md:w-1/4 mt-4">
+            <div className="bg-white overflow-hidden shadow rounded-lg mx-2">
+              <div className="px-4 py-5 sm:p-6   text-4xl font-semibold text-center">
+                {totalRoom}
+              </div>
+              <span className="px-4 py-1 bg-green-400 rounded-r text-white shadow">
+                Current
+              </span>
+              <div className="bg-gray-50 px-4 py-4 sm:px-6 text-center -mt-3">
+                Number of Rooms
+              </div>
+            </div>
+            <div className="bg-white overflow-hidden shadow rounded-lg mx-2 mt-4">
+              <div className="px-4 py-5 sm:p-6   text-4xl font-semibold text-center">
+                {roomUsed}
+              </div>
+              <span className="px-4 py-1 bg-blue-400 rounded-r text-white shadow">
+                Used
+              </span>
+              <div className="bg-gray-50 px-4 py-4 sm:px-6 text-center -mt-3">
+                Number of Rooms
+              </div>
+            </div>
+          </div>
+          <div className="w-full md:w-1/4 mt-4">
+            <div className="bg-white overflow-hidden shadow rounded-lg mx-2">
+              <div className="px-4 py-5 sm:p-6   text-4xl font-semibold text-center">
+                {totalBed}
+              </div>
+              <span className="px-4 py-1 bg-green-400 rounded-r text-white shadow">
+                Current
+              </span>
+              <div className="bg-gray-50 px-4 py-4 sm:px-6 text-center -mt-3">
+                Number of Beds
+              </div>
+            </div>
+            <div className="bg-white overflow-hidden shadow rounded-lg mx-2 mt-4">
+              <div className="px-4 py-5 sm:p-6   text-4xl font-semibold text-center">
+                {bedUsed}
+              </div>
+              <span className="px-4 py-1 bg-blue-400 rounded-r text-white shadow">
+                Used
+              </span>
+              <div className="bg-gray-50 px-4 py-4 sm:px-6 text-center -mt-3">
+                Number of Beds
+              </div>
             </div>
           </div>
         </div>
