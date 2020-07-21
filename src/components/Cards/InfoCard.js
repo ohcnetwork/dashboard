8bfed5031796b185850593b00af3797b7308f788
saanuregh <saanu09xs@gmail.com>
2020-07-21 19:18:49 +0530

    animated infocard

--- src/components/Cards/InfoCard.js
+++ src/components/Cards/InfoCard.js
@@ -1,46 +1,47 @@
 import React from "react";
 import { Card, CardBody } from "windmill-react-ui";
+import { animated, config, useSpring } from "react-spring";
 
-export function InfoCard({ title, value, delta = null }) {
+export function InfoCard({ title, value, delta = null, small = false }) {
+  const { _value, _delta } = useSpring({
+    from: { _value: 0, _delta: 0 },
+    to: {
+      _value: value,
+      _delta: delta || 0,
+    },
+    delay: 0,
+    config: config.slow,
+  });
   return (
     <Card>
       <CardBody className="flex flex-col">
         <div>
-          <p className="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
+          <p
+            className={`${
+              small ? "mb-1 text-xs" : "mb-2 text-sm"
+            } font-medium text-gray-600 dark:text-gray-400`}
+          >
             {title}
           </p>
           <div className="flex">
-            <p className="text-lg font-semibold text-gray-700 dark:text-gray-200">
-              {value}
-            </p>
+            <animated.p
+              className={`${
+                small ? "text-base" : "text-lg"
+              } font-semibold text-gray-700 dark:text-gray-200`}
+            >
+              {_value.interpolate((x) => Math.round(x))}
+            </animated.p>
             {delta != null && (
-              <span className="ml-2 text-sm text-gray-600 dark:text-gray-400">
-                {delta == 0 ? "-" : delta > 0 ? `+${delta}` : delta}
-              </span>
-            )}
-          </div>
-        </div>
-      </CardBody>
-    </Card>
-  );
-}
-
-export function SmallInfoCard({ title, value, delta }) {
-  return (
-    <Card>
-      <CardBody className="flex flex-col">
-        <div>
-          <p className="mb-1 text-xs font-medium text-gray-600 dark:text-gray-400">
-            {title}
-          </p>
-          <div className="flex">
-            <p className="text-base font-semibold text-gray-700 dark:text-gray-200">
-              {value}
-            </p>
-            {delta != null && (
-              <span className="ml-2 text-xs text-gray-600 dark:text-gray-400">
-                {delta == 0 ? "-" : delta > 0 ? `+${delta}` : delta}
-              </span>
+              <animated.span
+                className={`ml-2 ${
+                  small ? "text-xs" : "text-sm"
+                } text-gray-600 dark:text-gray-400`}
+              >
+                {_delta.interpolate((y) => {
+                  let x = Math.round(y);
+                  return x == 0 ? "-" : x > 0 ? `+${x}` : x;
+                })}
+              </animated.span>
             )}
           </div>
         </div>
