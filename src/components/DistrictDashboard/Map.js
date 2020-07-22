fcda8d120ae1859f1a266ec7cf29a7e6894d6623
saanuregh <saanu09xs@gmail.com>
2020-07-23 02:42:27 +0530

    map tooltip added

--- src/components/DistrictDashboard/Map.js
+++ src/components/DistrictDashboard/Map.js
@@ -1,6 +1,7 @@
-import { Text } from "@vx/text";
+import { defaultStyles, TooltipWithBounds, useTooltip } from "@vx/tooltip";
+import { Card, CardBody, WindmillContext } from "@windmill/react-ui";
 import polylabel from "polylabel";
-import React, { createRef, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import {
   ComposableMap,
   Geographies,
@@ -8,15 +9,44 @@ import {
   Marker,
   ZoomableGroup,
 } from "react-simple-maps";
-import { Tooltip } from "react-svg-tooltip";
 import { feature } from "topojson";
-import { Card, CardBody } from "@windmill/react-ui";
+import { availabilityTypes } from "../../utils/constants";
 import { getDistrict, getLSGD } from "../../utils/utils";
 
 function Map({ district, facilities, className }) {
   const [topojson, setTopojson] = useState({});
+  const [zoom, setZoom] = useState(1);
   const [markers, setMarkers] = useState([]);
   const [projectionConfig, setProjectionConfig] = useState({});
+  const { mode } = useContext(WindmillContext);
+  const {
+    tooltipData,
+    tooltipLeft,
+    tooltipTop,
+    tooltipOpen,
+    showTooltip,
+    hideTooltip,
+  } = useTooltip();
+
+  const getColor = ({ color1 = "00FF00", color2 = "FF0000", ratio }) => {
+    const hex = (color) => {
+      const colorString = color.toString(16);
+      return colorString.length === 1 ? `0${colorString}` : colorString;
+    };
+    const r = Math.ceil(
+      parseInt(color2.substring(0, 2), 16) * ratio +
+        parseInt(color1.substring(0, 2), 16) * (1 - ratio)
+    );
+    const g = Math.ceil(
+      parseInt(color2.substring(2, 4), 16) * ratio +
+        parseInt(color1.substring(2, 4), 16) * (1 - ratio)
+    );
+    const b = Math.ceil(
+      parseInt(color2.substring(4, 6), 16) * ratio +
+        parseInt(color1.substring(4, 6), 16) * (1 - ratio)
+    );
+    return "#" + hex(r) + hex(g) + hex(b);
+  };
 
   useEffect(() => {
     getLSGD().then(({ data }) => {
@@ -35,9 +65,82 @@ function Map({ district, facilities, className }) {
     });
   }, []);
 
+  const genToolTip = (f) => (
+    <div className="flex flex-col">
+      <p className="mb-2 text-base font-semibold">{f.name}</p>
+      <div className="mb-1">
+        <p className="font-semibold">Oxygen capacity</p>
+        <p>
+          Current: <strong>{f.oxygenCapacity}</strong>
+        </p>
+      </div>
+      {Object.keys(availabilityTypes).map((a) => {
+        let current = f.capacity[a]?.current_capacity || 1;
+        let total = f.capacity[a]?.total_capacity || 1;
+        let used = ((current / total) * 100).toFixed(2);
+        return (
+          <div className="mb-1" id={a}>
+            <p className="font-semibold">{availabilityTypes[a]}</p>
+            {f.capacity[a] ? (
+              <>
+                <p>
+                  Current: <strong>{current}</strong>
+                </p>
+                <p>
+                  Total: <strong>{total}</strong>
+                </p>
+                <p>
+                  Used:{" "}
+                  <strong
+                    style={{
+                      color: getColor({
+                        ratio: current / total,
+                      }),
+                    }}
+                  >
+                    {used}%
+                  </strong>
+                </p>
+              </>
+            ) : (
+              <p>Not available</p>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+
   return (
     <Card className={className}>
-      <CardBody>
+      <CardBody className="relative">
+        <div className="absolute bottom-0 right-0 flex flex-col items-end p-5 space-y-1 text-xs">
+          <div
+            style={{ backgroundColor: "#00FF00" }}
+            className="w-12 h-6 p-1 leading-none text-center border border-black"
+          >
+            Full
+          </div>
+          <div
+            style={{ backgroundColor: "#FF0000" }}
+            className="w-12 h-6 p-1 leading-none text-center border border-black"
+          >
+            Empty
+          </div>
+          <div
+            style={{ backgroundColor: "gray" }}
+            className="w-12 h-6 p-1 leading-none text-center border border-black"
+          >
+            None
+          </div>
+          <div className="grid grid-cols-4 gap-0">
+            {Object.keys(availabilityTypes).map((a) => (
+              <div className="h-6 p-1 leading-none text-center bg-white border border-black">
+                {availabilityTypes[a]}
+              </div>
+            ))}
+          </div>
+        </div>
         {topojson.type && (
           <ComposableMap
             projection="geoMercator"
@@ -46,7 +149,10 @@ function Map({ district, facilities, className }) {
             }}
             height={400}
           >
-            <ZoomableGroup center={projectionConfig[district] || [0, 0]}>
+            <ZoomableGroup
+              center={projectionConfig[district] || [0, 0]}
+              onMoveEnd={({ zoom }) => setZoom(zoom / 2)}
+            >
               <Geographies
                 className="text-purple-600 fill-current dark:text-gray-400"
                 geography={topojson}
@@ -67,54 +173,68 @@ function Map({ district, facilities, className }) {
                     key={e.properties.id}
                     coordinates={polylabel(e.geometry.coordinates)}
                   >
-                    <Text fontSize={3} textAnchor="middle">
+                    <text fontSize={3} textAnchor="middle">
                       {e.properties.LSGD}
-                    </Text>
+                    </text>
                   </Marker>
                 ))}
               {facilities
                 .filter((f) => f.location)
-                .map(({ id, name, location }) => {
-                  const circleRef = createRef();
-                  return (
-                    <Marker
-                      key={id}
-                      coordinates={Object.values(location).reverse()}
-                    >
-                      <circle
-                        ref={circleRef}
-                        r={2}
-                        fill="#F00"
-                        stroke="#fff"
-                        strokeWidth={1}
-                      />
-                      <Tooltip triggerRef={circleRef}>
-                        <svg
-                          x={5}
-                          y={5}
-                          width={80}
-                          height="20"
-                          viewBox="0 0 80 20"
-                        >
-                          <rect width="100%" height="100%" fill="black" />
-                          <Text
-                            verticalAnchor="start"
-                            x={2}
-                            y={2}
-                            fill="white"
-                            fontSize="7"
-                            scaleToFit={true}
-                            width={80}
-                          >
-                            {`Name: ${name}`}
-                          </Text>
-                        </svg>
-                      </Tooltip>
-                    </Marker>
-                  );
-                })}
+                .map((f) => (
+                  <Marker
+                    key={f.id}
+                    coordinates={Object.values(f.location).reverse()}
+                    alignmentBaseline="middle"
+                    onMouseOver={(event) => {
+                      showTooltip({
+                        tooltipLeft: event.pageX,
+                        tooltipTop: event.pageY,
+                        tooltipData: event.currentTarget.firstChild.id,
+                      });
+                    }}
+                    onMouseOut={hideTooltip}
+                  >
+                    <g alignmentBaseline="middle" id={f.id}>
+                      {Object.keys(availabilityTypes).map((a, i) => {
+                        let j = f.capacity[a];
+                        const props = {
+                          transform: `translate(${(4 * i * 1) / zoom}, 0)`,
+                          height: (4 * 1) / zoom,
+                          width: (4 * 1) / zoom,
+                          stroke: "black",
+                          strokeWidth: (0.1 * 1) / zoom,
+                        };
+                        return j ? (
+                          <rect
+                            fill={getColor({
+                              ratio: j.current_capacity / j.total_capacity,
+                            })}
+                            {...props}
+                          />
+                        ) : (
+                          <rect {...props} fill="gray" />
+                        );
+                      })}
+                    </g>
+                  </Marker>
+                ))}
             </ZoomableGroup>
           </ComposableMap>
+        )}
+        {tooltipOpen && (
+          <TooltipWithBounds
+            key={Math.random()}
+            top={tooltipTop}
+            left={tooltipLeft}
+            style={{
+              ...defaultStyles,
+              minWidth: 60,
+              backgroundColor: mode === "dark" ? "#7e3af2" : "white",
+              color: mode === "dark" ? "white" : "#4c4f52",
+            }}
+          >
+            {genToolTip(facilities.find((x) => x.id == tooltipData))}
+          </TooltipWithBounds>
         )}
       </CardBody>
     </Card>
