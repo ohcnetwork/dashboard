85c5d8f06750080c1f924b5037f4b8693d4df378
saanuregh <saanu09xs@gmail.com>
2020-09-06 01:17:11 +0530

    replaced cookies with store.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { useMemo, useState } from "react";
-import Cookies from "js-cookie";
+import Store from "store";
 
 const ACCESS_TOKEN = "care_access_token";
 const REFRESH_TOKEN = "care_refresh_token";
@@ -7,22 +7,22 @@ const REFRESH_TOKEN = "care_refresh_token";
 export const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
-  let token = Cookies.get(ACCESS_TOKEN);
-  let refresh = Cookies.get(REFRESH_TOKEN);
+  let token = Store.get(ACCESS_TOKEN);
+  let refresh = Store.get(REFRESH_TOKEN);
   let logged = token && refresh ? true : false;
   let userData = {};
 
   const [auth, setAuth] = useState({ logged, token, refresh, userData });
 
   function login(token, refresh, userData) {
-    Cookies.set(ACCESS_TOKEN, token);
-    Cookies.set(REFRESH_TOKEN, refresh);
+    Store.set(ACCESS_TOKEN, token);
+    Store.set(REFRESH_TOKEN, refresh);
     setAuth({ logged: true, token, refresh, userData });
   }
 
   function logout() {
-    Cookies.remove(ACCESS_TOKEN);
-    Cookies.remove(REFRESH_TOKEN);
+    Store.remove(ACCESS_TOKEN);
+    Store.remove(REFRESH_TOKEN);
     setAuth({ logged: false });
   }
 
