1be89f951124e1fbc7f11ce65d7521ec60ad4daf
saanuregh <saanu09xs@gmail.com>
2020-09-06 01:51:45 +0530

    replaced axios by redaxios

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "redaxios";
 
 const API_BASE_URL = process.env.POI_APP_CARE_BASE_URL || "";
 const STAT_BASE_URL = "https://keralastats.coronasafe.live";
