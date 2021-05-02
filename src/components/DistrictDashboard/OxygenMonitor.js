b48f6e658a01468df404344083129637206eec2b
Bodhish Thomas <bodhish@gmail.com>
2021-05-02 21:32:09 +0530

    Update logic for time left

--- src/components/DistrictDashboard/OxygenMonitor.js
+++ src/components/DistrictDashboard/OxygenMonitor.js
@@ -67,7 +67,7 @@ const showStockWithBurnRate = (inventoryItem) => {
           <path d="M6.5 0a.5.5 0 0 0 0 1H7v1.07A7.001 7.001 0 0 0 8 16a7 7 0 0 0 5.29-11.584.531.531 0 0 0 .013-.012l.354-.354.353.354a.5.5 0 1 0 .707-.707l-1.414-1.415a.5.5 0 1 0-.707.707l.354.354-.354.354a.717.717 0 0 0-.012.012A6.973 6.973 0 0 0 9 2.071V1h.5a.5.5 0 0 0 0-1h-3zm2 5.6V9a.5.5 0 0 1-.5.5H4.5a.5.5 0 0 1 0-1h3V5.6a.5.5 0 1 1 1 0z" />
         </svg>
         <span className="pl-2 text-sm font-semibold">
-          {((inventoryItem?.stock * inventoryItem?.burn_rate) / 60).toFixed(2)}
+          {(inventoryItem?.stock / inventoryItem?.burn_rate).toFixed(2)}
         </span>
         <span className="pl-1 text-xs"> hr </span>
       </small>
@@ -113,6 +113,7 @@ function OxygenMonitor({ filterDistrict, filterFacilityTypes, date }) {
           Object.keys(c.inventory).length !== 0 &&
           (c.inventory[2] || c.inventory[4] || c.inventory[5] || c.inventory[6])
         ) {
+          console.log(c.inventory);
           const arr = [
             [
               [c.name, c.facilityType, c.phoneNumber],
