0096debfce19217d0fa93ca918f8995d5980950a
Bodhish Thomas <bodhish@gmail.com>
2021-05-14 01:31:26 +0530

    Update the structure of data capture

--- src/components/DistrictDashboard/Capacity.js
+++ src/components/DistrictDashboard/Capacity.js
@@ -12,6 +12,7 @@ import {
   AVAILABILITY_TYPES,
   AVAILABILITY_TYPES_ORDERED,
   AVAILABILITY_TYPES_PROXY,
+  AVAILABILITY_TYPES_TOTAL_ORDERED,
 } from "../../utils/constants";
 import {
   dateString,
@@ -43,12 +44,12 @@ const showBedsTypes = (ids, c) => {
       { title: AVAILABILITY_TYPES_PROXY[i], total, current, vacant },
     ];
   }, []);
-  const total = positiveVal(data[0].total - data[1].total);
-  const current = positiveVal(data[0].current - data[1].current);
-  const vacant = positiveVal(data[0].vacant - data[1].vacant);
-  const title = "Non-Covid";
+  const total = positiveVal(data[0].total + data[1].total);
+  const current = positiveVal(data[0].current + data[1].current);
+  const vacant = positiveVal(data[0].vacant + data[1].vacant);
+  const title = "Total";
   const critical = total > 0 && vacant / total < 0.2;
-  if (data[0].total === 0) return React.null;
+  if (data[0].total === 0 && data[1].total === 0) return React.null;
   return (
     <table className="table-auto w-full">
       <thead>
@@ -94,6 +95,10 @@ const initialFacilitiesTrivia = {
   110: { total: 0, used: 0 },
   120: { total: 0, used: 0 },
   30: { total: 0, used: 0 },
+  1111: { total: 0, used: 0 },
+  2222: { total: 0, used: 0 },
+  3333: { total: 0, used: 0 },
+  4444: { total: 0, used: 0 },
   actualDischargedPatients: 0,
   actualLivePatients: 0,
   count: 0,
@@ -130,6 +135,17 @@ function Capacity({ filterDistrict, filterFacilityTypes, date }) {
           a[key][k].used += c.capacity[k]?.current_capacity || 0;
           a[key][k].total += c.capacity[k]?.total_capacity || 0;
         });
+
+        AVAILABILITY_TYPES_TOTAL_ORDERED.forEach((k) => {
+          let current_covid = c.capacity[k.covid]?.current_capacity || 0;
+          let current_non_covid =
+            c.capacity[k.non_covid]?.current_capacity || 0;
+          let total_covid = c.capacity[k.covid]?.total_capacity || 0;
+          let total_non_covid = c.capacity[k.non_covid]?.total_capacity || 0;
+          a[key][k.id].used += current_covid + current_non_covid;
+          a[key][k.id].total += total_covid + total_non_covid;
+        });
+
         return a;
       },
       {
@@ -233,6 +249,19 @@ function Capacity({ filterDistrict, filterFacilityTypes, date }) {
             </Button>
           </Pill>
         </div>
+
+        <div className="grid-col-1 grid gap-6 mb-8 md:grid-cols-4">
+          {AVAILABILITY_TYPES_TOTAL_ORDERED.map((k) => (
+            <RadialCard
+              label={k.name}
+              count={facilitiesTrivia.current.count}
+              current={facilitiesTrivia.current[k.id]}
+              previous={facilitiesTrivia.previous[k.id]}
+              key={k.name}
+            />
+          ))}
+        </div>
+
         <div className="grid-col-1 grid gap-6 mb-8 md:grid-cols-4">
           {AVAILABILITY_TYPES_ORDERED.map((k) => (
             <RadialCard
