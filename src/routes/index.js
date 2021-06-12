9f0c3b6a1e824551d70bcd54b9e3cd4357f1be0b
Bodhish Thomas <bodhish@gmail.com>
2021-06-12 14:12:06 +0530

    Revert "Add facility page"

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
 import { lazy } from "react";
 
 const DistrictDashboard = lazy(() => import("../pages/DistrictDashboard"));
-const Facility = lazy(() => import("../pages/Facility"));
 const Page404 = lazy(() => import("../pages/404"));
 
 const routes = [
@@ -16,10 +15,6 @@ const routes = [
   {
     path: "/404",
     component: Page404,
-  },
-  {
-    path: "/facility/:facilityId",
-    component: Facility,
   },
 ];
 
