c47df8e5e09b584d19766f12da50e06d489575b8
ProCode2 <bpro249@gmail.com>
2021-06-14 19:54:14 +0530

    refcator: added expected burn rate component for facility

--- src/pages/Facility.js
+++ src/pages/Facility.js
@@ -12,33 +12,12 @@ import {
 
 import {
   FACILITY_TYPES,
-  PATIENT_TYPES,
-  OXYGEN_TYPES_KEYS,
   ACTIVATED_DISTRICTS,
 } from "../utils/constants";
 import Capacity from "../components/Facility/Capacity";
 import Patients from "../components/Facility/Patients";
 import Oxygen from "../components/Facility/Oxygen";
-
-
-function useQuery() {
-  return new URLSearchParams(useLocation().search);
-}
-
-
-const initialPatientFacilitiesTrivia = {
-  count: 0,
-  icu: { total: 0, today: 0 },
-  oxygen_bed: { total: 0, today: 0 },
-  not_admitted: { total: 0, today: 0 },
-  home_isolation: { total: 0, today: 0 },
-  isolation_room: { total: 0, today: 0 },
-  home_quarantine: { total: 0, today: 0 },
-  paediatric_ward: { total: 0, today: 0 },
-  gynaecology_ward: { total: 0, today: 0 },
-  icu_with_invasive_ventilator: { total: 0, today: 0 },
-  icu_with_non_invasive_ventilator: { total: 0, today: 0 },
-};
+import ExpectedBurnRate from "../components/Facility/ExpectedBurnRate";
 
 function Facility() {
   const params = useParams();
@@ -142,154 +121,7 @@ function Facility() {
         <Capacity filtered={filtered} date={date} />
         <Patients patientsFiltered={patientsFiltered} date={date} />
         <Oxygen oxygenData={oxygenData} />
-        <section className="my-8 px-6 py-4 dark:bg-gray-700 bg-white">
-          <h2 className="text-green-500 text-lg font-bold">
-            Expected Burn Rate
-          </h2>
-          <div className="grid-col-1 grid gap-6 mb-4 mt-8 md:grid-cols-3">
-            {patientData.results[0].data
-              .total_patients_bed_with_oxygen_support !== 0 && (
-                <div
-                  key={"OXYGEN_BED"}
-                  className="word-wrap flex flex-col pb-3 pl-3 pr-4 pt-6 break-words bg-gray-50 dark:bg-gray-800 rounded-md space-x-3"
-                >
-                  <div>
-                    <p className="ml-4 dark:text-gray-300 text-gray-500 text-lg font-semibold capitalize">
-                      {"Oxygen Bed"}
-                    </p>
-                  </div>
-                  <div className="flex items-center space-x-3">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      aria-hidden="true"
-                      focusable="false"
-                      data-prefix="fas"
-                      data-icon="fire"
-                      className="w-10 h-10 text-orange-500"
-                      role="img"
-                      viewBox="0 0 384 512"
-                    >
-                      <path
-                        fill="currentColor"
-                        d="M216 23.86c0-23.8-30.65-32.77-44.15-13.04C48 191.85 224 200 224 288c0 35.63-29.11 64.46-64.85 63.99-35.17-.45-63.15-29.77-63.15-64.94v-85.51c0-21.7-26.47-32.23-41.43-16.5C27.8 213.16 0 261.33 0 320c0 105.87 86.13 192 192 192s192-86.13 192-192c0-170.29-168-193-168-296.14z"
-                      />
-                    </svg>
-                    <h1 className="mb-2 mt-3 text-gray-800 dark:text-white text-3xl font-bold">
-                      {patientData.results[0].data
-                        .total_patients_bed_with_oxygen_support *
-                        7.4 *
-                        8.778}
-                      <span className="ml-1 text-lg font-bold">
-                        m<sup>3</sup>/hr
-                      </span>
-                      {
-                        <sup className="ml-1 dark:text-gray-500 text-gray-600 text-lg">
-                          {`${patientData.results[0].data.total_patients_bed_with_oxygen_support}`}
-                        </sup>
-                      }
-                    </h1>
-                  </div>
-                </div>
-              )}
-            {patientData.results[0].data
-              .total_patients_icu_with_oxygen_support !== 0 && (
-                <div
-                  key={"ICU"}
-                  className="word-wrap flex flex-col pb-3 pl-3 pr-4 pt-6 break-words bg-gray-50 dark:bg-gray-800 rounded-md space-x-3"
-                >
-                  <div>
-                    <p className="ml-4 dark:text-gray-300 text-gray-500 text-lg font-semibold capitalize">
-                      {"ICU"}
-                    </p>
-                  </div>
-                  <div className="flex items-center space-x-3">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      aria-hidden="true"
-                      focusable="false"
-                      data-prefix="fas"
-                      data-icon="fire"
-                      className="w-10 h-10 text-orange-500"
-                      role="img"
-                      viewBox="0 0 384 512"
-                    >
-                      <path
-                        fill="currentColor"
-                        d="M216 23.86c0-23.8-30.65-32.77-44.15-13.04C48 191.85 224 200 224 288c0 35.63-29.11 64.46-64.85 63.99-35.17-.45-63.15-29.77-63.15-64.94v-85.51c0-21.7-26.47-32.23-41.43-16.5C27.8 213.16 0 261.33 0 320c0 105.87 86.13 192 192 192s192-86.13 192-192c0-170.29-168-193-168-296.14z"
-                      />
-                    </svg>
-                    <h1 className="mb-2 mt-3 text-gray-800 dark:text-white text-3xl font-bold">
-                      {patientData.results[0].data
-                        .total_patients_icu_with_oxygen_support *
-                        10 *
-                        8.778}
-                      <span className="ml-1 text-lg font-bold">
-                        m<sup>3</sup>/hr
-                      </span>
-                      {
-                        <sup className="ml-1 dark:text-gray-500 text-gray-600 text-lg">
-                          {`${patientData.results[0].data.total_patients_icu_with_oxygen_support}`}
-                        </sup>
-                      }
-                    </h1>
-                  </div>
-                </div>
-              )}
-            {patientData.results[0].data
-              .total_patients_icu_with_invasive_ventilator +
-              patientData.results[0].data
-                .total_patients_icu_with_non_invasive_ventilator !==
-              0 && (
-                <div
-                  key={"Ventilator"}
-                  className="word-wrap flex flex-col pb-3 pl-3 pr-4 pt-6 break-words bg-gray-50 dark:bg-gray-800 rounded-md space-x-3"
-                >
-                  <div>
-                    <p className="ml-4 dark:text-gray-300 text-gray-500 text-lg font-semibold capitalize">
-                      {"Ventilator"}
-                    </p>
-                  </div>
-                  <div className="flex items-center space-x-3">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      aria-hidden="true"
-                      focusable="false"
-                      data-prefix="fas"
-                      data-icon="fire"
-                      className="w-10 h-10 text-orange-500"
-                      role="img"
-                      viewBox="0 0 384 512"
-                    >
-                      <path
-                        fill="currentColor"
-                        d="M216 23.86c0-23.8-30.65-32.77-44.15-13.04C48 191.85 224 200 224 288c0 35.63-29.11 64.46-64.85 63.99-35.17-.45-63.15-29.77-63.15-64.94v-85.51c0-21.7-26.47-32.23-41.43-16.5C27.8 213.16 0 261.33 0 320c0 105.87 86.13 192 192 192s192-86.13 192-192c0-170.29-168-193-168-296.14z"
-                      />
-                    </svg>
-                    <h1 className="mb-2 mt-3 text-gray-800 dark:text-white text-3xl font-bold">
-                      {(patientData.results[0].data
-                        .total_patients_icu_with_invasive_ventilator +
-                        patientData.results[0].data
-                          .total_patients_icu_with_non_invasive_ventilator) *
-                        10 *
-                        8.778}
-                      <span className="ml-1 text-lg font-bold">
-                        m<sup>3</sup>/hr
-                      </span>
-                      {
-                        <sup className="ml-1 dark:text-gray-500 text-gray-600 text-lg">
-                          {patientData.results[0].data
-                            .total_patients_icu_with_invasive_ventilator +
-                            patientData.results[0].data
-                              .total_patients_icu_with_non_invasive_ventilator}
-                        </sup>
-                      }
-                    </h1>
-                  </div>
-                </div>
-              )}
-          </div>
-        </section>
-
+        <ExpectedBurnRate patientData={patientData} />
         <section className="my-8 px-6 py-4 dark:bg-gray-700 bg-white">
           <h2 className="text-green-500 text-lg font-bold">Map</h2>
           <GMap
